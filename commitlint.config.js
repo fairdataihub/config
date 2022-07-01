28d8a4efcace97e65cc0712c8e3348a3dc35a674
Sanjay Soundarajan <sanjay.16@live.com>
2022-07-01 13:09:34 -0700

    chore: 🔧 update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,34 +1,7 @@
-const types = [
-  "build",
-  "chore",
-  "docs",
-  "feat",
-  "fix",
-  "perf",
-  "refactor",
-  "revert",
-  "style",
-  "test",
-  "ci",
-];
+import { commitlintConfig } from "@fairdataihub/config";
 
-const scopes = [
-  "app",
-  "library",
-  "backend",
-  "authentication",
-  "settings",
-  "deps",
-];
+const config = commitlintConfig();
 
-module.exports = {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [2, "always", types],
-    "scope-case": [2, "always", ["camel-case"]],
-    "scope-enum": [2, "always", scopes],
-    "subject-empty": [2, "never"],
-    "subject-case": [2, "always", ["lower-case"]],
-    "header-max-length": [2, "always", 72],
-  },
-};
+console.log(config);
+
+module.exports = config;
