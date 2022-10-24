5b5c03067e9431af747b05912a2454c69b9758a4
Sanjay Soundarajan <sanjay.16@live.com>
2022-10-24 12:27:04 -0700

    dx: 🧑‍💻️ update devmoji config

--- devmoji.config.js
+++ devmoji.config.js
@@ -1,5 +1,13 @@
-const { devmojiConfig } = require("@fairdataihub/config");
+const devmojiConfig = require("./src/data/commitInfo.json");
 
-const config = devmojiConfig();
+const types = [];
+
+for (const item of devmojiConfig.types) {
+  types.push(item.code);
+}
+const config = {
+  types,
+  devmoji: devmojiConfig.types,
+};
 
 module.exports = config;
